f98ed763d487563c9e2f609e54ea8cbb10889b40
Prodeveder <72999777+Prodeveder@users.noreply.github.com>
2024-05-08 11:21:36 +0200

    Add files via upload

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,16 +1,68 @@
+const headerContent = document.querySelectorAll(".header__container");
+const headerContentWrapper = document.querySelectorAll(
+  ".header__content__wrapper"
+);
 
+const clearHeader = () => {
+  headerContent.forEach((Element) => {
+    Element.style.display = "none";
+  });
+  headerContentWrapper.forEach((Element) => {
+    Element.classList.remove("active");
+  });
+};
 
-// SHow Menu 
+headerContent[0].style.display = "block";
 
-const menWrapper = document.querySelector('.mobile__navbar__wrapper');
-const openMenu = document.querySelector('.menu__open');
-const closeMenu = document.querySelector('.mobile__menu__close');
+const next = document.querySelector(".next");
+const prev = document.querySelector(".prev");
 
+let headerIndex = 0;
 
-openMenu.addEventListener('click', ()=> {
-    menWrapper.style.display = 'block';
-})
+const runSlider = () => {
+  setInterval(nextSlide, 5000);
+};
 
-closeMenu.addEventListener('click', ()=> {
-    menWrapper.style.display = 'none';
-})
+const nextSlide = () => {
+  clearHeader();
+  clearInterval();
+  if (headerIndex > 1) {
+    headerIndex = 0;
+  } else {
+    headerIndex += 1;
+  }
+  headerContent[headerIndex].style.display = "block";
+  headerContentWrapper[headerIndex].classList.add("active");
+};
+
+const prevSlide = () => {
+  clearHeader();
+  clearInterval();
+  if (headerIndex == 0) {
+    headerIndex = 2;
+  } else {
+    headerIndex -= 1;
+  }
+  headerContent[headerIndex].style.display = "block";
+  headerContentWrapper[headerIndex].classList.add("active");
+};
+
+runSlider();
+
+const menu = document.querySelector(".menu");
+
+const closeMenu = document.querySelector(".close");
+const mobileMenu = document.querySelector(".mobile__navbar__wrapper");
+menu.addEventListener("click", () => {
+  if (mobileMenu.style.display == "") {
+    mobileMenu.style.display = "flex";
+  } else if (mobileMenu.style.display == "none") {
+    mobileMenu.style.display = "flex";
+  } else {
+    mobileMenu.style.display = "none";
+  }
+});
+
+closeMenu.addEventListener("click", () => {
+  mobileMenu.style.display = "none";
+});
