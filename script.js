6ed4ceec359ba198b8bb5706e7b39f2c53bdb66d
Prodeveder <72999777+Prodeveder@users.noreply.github.com>
2024-07-15 09:45:27 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("JavaScript is ready to run");
+const menu = document.querySelector(".menu"),
+  close = document.querySelector(".close"),
+  mobileNav = document.querySelector(".mobile__nav__wrapper");
+
+menu.addEventListener("click", () => {
+  mobileNav.style.display = "block";
+});
+
+close.addEventListener("click", () => {
+  mobileNav.style.display = "none";
+});
